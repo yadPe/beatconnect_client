901eff86338195fd7838260487fd2c898563124e
YadPe <petitot.py@gmail.com>
2019-07-18 00:40:02 +0200

    new text input

---
+++ src/App/components/common/TextInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import injectSheet from 'react-jss'
+
+const styles = {
+  TextInput: {
+    padding: '2px 10px 3px',
+    lineHeight: '32px',
+    border: '2px solid rgba(255, 255, 255, 0.41)',
+    backgroundColor: 'transparent',
+    color: 'white',
+    '&:focus': {
+      outline: 'none !important',
+      borderWidth: '2px',
+      borderStyle: 'solid',
+      borderColor: props => props.color,
+      backgroundColor: 'white',
+      color: 'black',
+    },
+    '&:hover': {
+      outline: 'none !important',
+      border: '2px solid white',
+    }
+  }
+}
+
+const TextInput = ({ type, placeholder, value, onChange, classes }) => (
+  <input className={classes.TextInput} type={type || 'text'} placeholder={placeholder || ''} value={value} onChange={onChange} />
+);
+
+export default injectSheet(styles)(TextInput);
