06db1650fc270223235a3685fd6cd59722125430
YadPe <petitot.py@gmail.com>
2019-09-03 23:02:30 +0200

    WIP Beatmaps grid virtualization

--- src/App/components/Beatmaps/index.js
+++ src/App/components/Beatmaps/index.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { FixedSizeGrid } from 'react-window';
 import injectSheet from 'react-jss';
 import Beatmap from '../common/Beatmap'
 import Search from './Search';
+// https://blog.logrocket.com/rendering-large-lists-with-react-virtualized-82741907a6b3/
+// https://web.dev/virtualize-long-lists-react-window
 
 const styles = {
   list: {
@@ -10,45 +13,46 @@ const styles = {
     alignItems: 'center',
     gridTemplateColumns: 'repeat(auto-fit, minmax(700px, 1fr))',
     //gridGap: '10px'
+  },
+  item: {
+    // margin: '10%'
   }
 };
 
-const Beatmaps = ({ theme, searchResults, classes, setHeaderContent }) => {
+const Beatmaps = ({ theme, searchResults, classes, setHeaderContent, window, panelExpended }) => {
   const { search, beatmaps } = searchResults
+  const displayGrid = window.width - (panelExpended ? 150 : 44) >= 700;
 
   useEffect(() => {
     setHeaderContent(<Search theme={theme} lastSearch={search} />)
     return () => setHeaderContent(null)
   }, [setHeaderContent, search, theme])
 
-
-  // const renderBeatmaps = () => {
-  //   return beatmaps.map((beatmap, i) => {
-  //     return beatmaps.length - i === 5 ?
-  //       <VizSensor
-  //         onChange={() => console.log('yeeeeeaa')}
-  //         key={`fetchTrigger${beatmap.beatmapset_id || beatmap.id}`}>
-  //         <Beatmap theme={theme} beatmap={beatmap} key={`beatmap${beatmap.beatmapset_id || beatmap.id}`} />
-  //       </VizSensor> :
-  //       <Beatmap theme={theme} beatmap={beatmap} key={`beatmap${beatmap.beatmapset_id || beatmap.id}`} />
-  //   })
-  // }
-
-  const renderBeatmaps = () => {
-    return beatmaps.map((beatmap, i) => {
-      return <Beatmap theme={theme} beatmap={beatmap} key={`beatmap${beatmap.beatmapset_id || beatmap.id}`} />
-    })
+  const renderBeatmaps = ({ columnIndex, rowIndex, style }) => {
+    return (
+      <div style={style} className={classes.item}>
+        <Beatmap width='90%' theme={theme} beatmap={beatmaps[(rowIndex === 0 ? 0 : rowIndex + rowIndex) + (columnIndex)]} key={`beatmap${beatmaps[(rowIndex === 0 ? 0 : rowIndex + rowIndex) + (columnIndex)].beatmapset_id || beatmaps[(rowIndex === 0 ? 0 : rowIndex + rowIndex) + (columnIndex)].id}`} />
+      </div>
+    )
   }
 
-
+  console.log((window.width / 2) - 100)
   return (
     <div className='Beatmaps' >
-      <div className={classes.list}>
-        {renderBeatmaps()}
-      </div>
+      <FixedSizeGrid
+        columnCount={displayGrid ? 2 : 1}
+        columnWidth={(window.width / 2) - 100}
+        rowCount={displayGrid ? beatmaps.length / 2 : beatmaps.length}
+        rowHeight={222}
+        overscanCount={10}
+        height={window.height - 79}
+        width={(window.width - (panelExpended ? 150 : 44))}
+      >
+        {renderBeatmaps}
+      </FixedSizeGrid>
     </div>
   );
 }
 
-const mapStateToProps = ({ main }) => ({ searchResults: main.searchResults })
+const mapStateToProps = ({ main, settings }) => ({ searchResults: main.searchResults, window: main.window, panelExpended: settings.userPreferences.sidePanelExpended })
 export default connect(mapStateToProps)(injectSheet(styles)(Beatmaps));
