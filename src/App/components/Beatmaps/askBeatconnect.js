ebbf8806b7af6a29e208aff4f3801fae6ddf928c
YadPe <petitot.py@gmail.com>
2019-09-04 21:08:43 +0200

    WIP infiniteLoader for Beatmaps

--- src/App/components/Beatmaps/askBeatconnect.js
+++ src/App/components/Beatmaps/askBeatconnect.js
@@ -2,11 +2,13 @@ import store from '../../../store';
 
 const askBeatconnect = (search, onLoading) => {
   const { query, page, status, mode } = search;
+  const prevBeatmaps = store.getState().main.searchResults.beatmaps
   onLoading(true)
   const formatQuery = query.split(' ').join('%20')
   fetch(`https://beatconnect.io/api//search/?token=b3z8gl9pzt7iqa89&p=${page || 0}&q=${formatQuery}&s=${status || 'ranked'}&m=${mode || 'all'}`)
     .then(res => res.json())
     .then(({ beatmaps, max_page }) => {
+      if (page > 0) beatmaps = [...prevBeatmaps, ...beatmaps]
       store.dispatch({ type: 'SEARCH_RESULTS', searchResults: { search, beatmaps, max_page, page }})
       onLoading(false)
     })
--- src/App/components/Beatmaps/index.js
+++ src/App/components/Beatmaps/index.js
@@ -4,8 +4,13 @@ import { FixedSizeGrid } from 'react-window';
 import injectSheet from 'react-jss';
 import Beatmap from '../common/Beatmap'
 import Search from './Search';
+import askBeatconnect from './askBeatconnect';
+import InfiniteLoader from "react-window-infinite-loader";
+import {convertFromGridToInfiniteProps as onItemsRendered} from './convertFromGridToInfiniteProps';
 // https://blog.logrocket.com/rendering-large-lists-with-react-virtualized-82741907a6b3/
 // https://web.dev/virtualize-long-lists-react-window
+// https://github.com/bvaughn/react-window-infinite-loader/issues/3
+// 
 
 const styles = {
   list: {
@@ -17,10 +22,16 @@ const styles = {
 };
 
 const Beatmaps = ({ theme, searchResults, classes, setHeaderContent, window, panelExpended }) => {
-  const { search, beatmaps } = searchResults
+  const { search, beatmaps, max_page } = searchResults
   const gridWidth = (window.width - (panelExpended ? 150 : 48))
   const gridHeight = window.height - 79
   const displayGrid = gridWidth >= 960;
+  console.log(max_page)
+  const canLoadMore = search.page < max_page
+  const loadMore = () => {
+    console.log('more')
+    askBeatconnect({ ...search, page: search.page += 1 })
+  }
 
   useEffect(() => {
     setHeaderContent(<Search theme={theme} lastSearch={search} />)
@@ -30,27 +41,71 @@ const Beatmaps = ({ theme, searchResults, classes, setHeaderContent, window, pan
   const renderBeatmaps = ({ columnIndex, rowIndex, style }) => {
     return (
       <div style={style}>
-        {displayGrid ? 
-        <Beatmap width='90%' theme={theme} beatmap={beatmaps[(rowIndex === 0 ? 0 : rowIndex + rowIndex) + (columnIndex)]} key={`beatmap${beatmaps[(rowIndex === 0 ? 0 : rowIndex + rowIndex) + (columnIndex)].beatmapset_id || beatmaps[(rowIndex === 0 ? 0 : rowIndex + rowIndex) + (columnIndex)].id}`} />
-      :
-      <Beatmap width='90%' theme={theme} beatmap={beatmaps[rowIndex]} key={`beatmap${beatmaps[rowIndex].beatmapset_id || beatmaps[rowIndex].id}`} />}
+        {displayGrid ?
+          <Beatmap width='90%' theme={theme} beatmap={beatmaps[(rowIndex === 0 ? 0 : rowIndex + rowIndex) + (columnIndex)]} key={`beatmap${beatmaps[(rowIndex === 0 ? 0 : rowIndex + rowIndex) + (columnIndex)].beatmapset_id || beatmaps[(rowIndex === 0 ? 0 : rowIndex + rowIndex) + (columnIndex)].id}`} />
+          :
+          <Beatmap width='90%' theme={theme} beatmap={beatmaps[rowIndex]} key={`beatmap${beatmaps[rowIndex].beatmapset_id || beatmaps[rowIndex].id}`} />}
       </div>
     )
   }
 
   return (
     <div className='Beatmaps'>
-      <FixedSizeGrid
-        columnCount={displayGrid ? 2 : 1}
-        columnWidth={displayGrid ? (gridWidth / 2)-9 : gridWidth - 18}
-        rowCount={displayGrid ? beatmaps.length / 2 : beatmaps.length}
-        rowHeight={250}
-        overscanCount={10}
-        height={gridHeight}
-        width={gridWidth}
+      <InfiniteLoader
+      isItemLoaded={index => index < beatmaps.length}
+      loadMoreItems={loadMore}
+      itemCount={beatmaps.length}
       >
-        {renderBeatmaps}
-      </FixedSizeGrid>
+        {({ onItemsRendered, ref }) => {
+          const newItemsRendered = gridData => {
+            const {
+              visibleRowStartIndex,
+              visibleRowStopIndex,
+              visibleColumnStopIndex,
+              overscanRowStartIndex,
+              overscanRowStopIndex,
+              overscanColumnStopIndex
+            } = gridData;
+
+            const endCol =
+              (this.props.useOverscanForLoading || true
+                ? overscanColumnStopIndex
+                : visibleColumnStopIndex) + 1;
+
+            const startRow =
+              this.props.useOverscanForLoading || true
+                ? overscanRowStartIndex
+                : visibleRowStartIndex;
+            const endRow =
+              this.props.useOverscanForLoading || true
+                ? overscanRowStopIndex
+                : visibleRowStopIndex;
+
+            const visibleStartIndex = startRow * endCol;
+            const visibleStopIndex = endRow * endCol;
+
+            onItemsRendered({
+              //call onItemsRendered from InfiniteLoader so it can load more if needed
+              visibleStartIndex,
+              visibleStopIndex
+            });
+          };
+          return (
+          <FixedSizeGrid
+            columnCount={displayGrid ? 2 : 1}
+            columnWidth={displayGrid ? (gridWidth / 2) - 9 : gridWidth - 18}
+            rowCount={displayGrid ? beatmaps.length / 2 : beatmaps.length}
+            rowHeight={250}
+            overscanCount={10}
+            height={gridHeight}
+            width={gridWidth}
+            onItemsRendered={newItemsRendered}
+            ref={ref}
+          >
+            {renderBeatmaps}
+          </FixedSizeGrid>
+        )}}
+      </InfiniteLoader>
     </div>
   );
 }
