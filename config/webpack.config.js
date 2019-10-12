c55699828491bb439d6312d4b3ad4b2a2c3d601b
Yannis Petitot <46972108+yadPe@users.noreply.github.com>
2019-10-12 22:32:55 +0200

    Fix(App): osu! beatmaps sync (#51)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -309,7 +309,7 @@ module.exports = function(webpackEnv) {
               options: {
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
-                
+                emitWarning: true,
               },
               loader: require.resolve('eslint-loader'),
             },
