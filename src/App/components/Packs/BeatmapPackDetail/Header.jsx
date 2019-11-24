76a2b5a917050e09afcd35e261beb381737bc519
Yannis Petitot <46972108+yadPe@users.noreply.github.com>
2019-11-24 20:20:09 +0100

    Download only filtered beatmaps from pack (#97)
    Closes #97

--- src/App/components/Packs/BeatmapPackDetail/Header.jsx
+++ src/App/components/Packs/BeatmapPackDetail/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import InjectSheet from 'react-jss';
 import TextInput from '../../common/TextInput';
 import renderIcons from '../../../utils/renderIcons';
@@ -33,20 +33,28 @@ const styles = {
 
 const PackDetailHeader = ({ classes, pack: { beatmapsets, name, type }, filter: { filter, setFilter }, quit }) => {
   const history = useContext(HistoryContext);
-  const handleInputChange = e => setFilter(e.target.value);
+  const [search, setSearch] = useState('');
+  if (search !== '') setFilter(search);
+  const handleInputChange = e => setSearch(e.target.value);
   const beatmapsToDownload = beatmapsets.filter(beatmap => !history.contains(beatmap.id));
+  const filteredBeatmapsets =
+    search !== ''
+      ? beatmapsToDownload.filter(
+          ({ title, artist }) =>
+            title.toLowerCase().includes(search.toLowerCase()) || artist.toLowerCase().includes(search.toLowerCase()),
+        )
+      : beatmapsToDownload;
   const packCompleted = beatmapsToDownload.length === 0;
-  const downloadTitle = packCompleted ? "You're all set !" : `Download ${beatmapsToDownload.length} beatmaps`;
+  const downloadTitle = packCompleted ? "You're all set !" : `Download ${filteredBeatmapsets.length} beatmaps`;
   return (
     <div className={classes.wrapper}>
       <div title="Back" role="button" onClick={quit} className={classes.backButton}>
         {renderIcons({ name: 'Back' })}
       </div>
       <p className={classes.name}>{name}</p>
-      {/* <p>{beatmapsets.length}</p> */}
-      {!packCompleted && <p title={downloadTitle}>{beatmapsToDownload.length}</p>}
+      {!packCompleted && <p title={downloadTitle}>{filteredBeatmapsets.length}</p>}
       <DownloadBeatmapBtn
-        pack={beatmapsToDownload}
+        pack={filteredBeatmapsets}
         title={downloadTitle}
         noStyle
         className={`${classes.downloadButton} clickable`}
