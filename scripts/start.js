34e25829b6db76357697022bb334307144fb4d11
YadPe <yannis.petitot@link-value.fr>
2019-12-10 22:49:40 +0100

    unused import

--- scripts/start.js
+++ scripts/start.js
@@ -20,7 +20,6 @@ const fs = require('fs');
 const chalk = require('react-dev-utils/chalk');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const clearConsole = require('react-dev-utils/clearConsole');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const {
   choosePort,
