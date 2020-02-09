36d02774adae2a6b4284682f880c5b6d5ea7f78f
YadPe <petitot.py@gmail.com>
2020-02-10 00:00:24 +0100

    Add message of disabled packs

--- src/App/modules/Packs/reducer/reducer.js
+++ src/App/modules/Packs/reducer/reducer.js
@@ -1,4 +1,7 @@
+import config from '../../../../shared/config';
+
 const initialState = {
+  serviceStatus: config.packs.serviceStatus.ok,
   packsDashboardData: {
     lastWeekOverview: [0, 0, 0, 0],
     std: {},
@@ -12,6 +15,8 @@ export default (state = initialState, { type, payload }) => {
   switch (type) {
     case 'PACKS_DASHBOARD_QUERY_DATA':
       return { ...state, packsDashboardData: { ...state.packsDashboardData, ...payload } };
+    case 'PACKS_DISABLED':
+      return { ...state, serviceStatus: payload };
     default:
       return state;
   }
