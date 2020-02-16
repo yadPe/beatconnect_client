5ae033909ea921bc53f79958238690d5f77d5870
YadPe <petitot.py@gmail.com>
2020-02-16 14:45:34 +0100

    Helpers refactor

--- src/App/helpers/timeSince.js
+++ src/App/helpers/timeSince.js
@@ -1,22 +1,20 @@
 export default timeStamp => {
-  const now = new Date(),
-    secondsPast = (now.getTime() - timeStamp.getTime()) / 1000;
+  const now = new Date();
+  const secondsPast = (now.getTime() - timeStamp.getTime()) / 1000;
   if (secondsPast < 60) {
-    return parseInt(secondsPast) + ' sec ago';
+    return `${parseInt(secondsPast, 10)} sec ago`;
   }
   if (secondsPast < 3600) {
-    return parseInt(secondsPast / 60) + ' min ago';
+    return `${parseInt(secondsPast / 60, 10)} min ago`;
   }
   if (secondsPast <= 86400) {
-    return parseInt(secondsPast / 3600) + ' h ago';
+    return `${parseInt(secondsPast / 3600, 10)} h ago'`;
   }
-  if (secondsPast > 86400) {
-    const day = timeStamp.getDate();
-    const month = timeStamp
-      .toDateString()
-      .match(/ [a-zA-Z]*/)[0]
-      .replace(' ', '');
-    const year = timeStamp.getFullYear() === now.getFullYear() ? '' : ' ' + timeStamp.getFullYear();
-    return day + ' ' + month + year;
-  }
+  const day = timeStamp.getDate();
+  const month = timeStamp
+    .toDateString()
+    .match(/ [a-zA-Z]*/)[0]
+    .replace(' ', '');
+  const year = timeStamp.getFullYear() === now.getFullYear() ? '' : ` ${timeStamp.getFullYear()}`;
+  return `${day} ${month + year}`;
 };
