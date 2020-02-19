0904ef01ca5e5ed9a8e92927845201d6ee858519
YadPe <petitot.py@gmail.com>
2020-02-19 08:24:58 +0100

    WEIip

--- src/electron/helpers/wallpaper.js
+++ src/electron/helpers/wallpaper.js
@@ -1,29 +1,33 @@
-const { existsSync, writeFile } = require('fs');
-const { normalize } = require('path');
-const wallpaper = require('wallpaper');
-const { request } = require('https');
-const Stream = require('stream').Transform;
+const { app } = require('electron');
+const { createWriteStream } = require('fs');
+const { set } = require('wallpaper');
+const { get } = require('https');
+const { join } = require('path');
 
-const downloadBg = uri =>
+const downloadAndSetBg = uri =>
   new Promise((resolve, reject) => {
-    request(uri, res => {
-      const data = new Stream();
-      res.on('data', data.push);
-      res.on('end', () => resolve(writeFile('bg.jpg', data.read())));
+    const savePath = join(app.getPath('temp'), 'bg.jpg');
+    const file = createWriteStream(savePath);
+    const request = get(uri, res => {
+      res.pipe(file);
+      res.once('end', () => {
+        if (res.statusCode !== 200) {
+          file.destroy();
+          reject(new Error(`Status code ${res.statusCode}`));
+        } else file.close();
+      });
     });
+    file.once('close', () => {
+      resolve(set(savePath));
+    });
+    request.on('error', reject);
   });
 
-const setWallpaper = imagePath => {
-  if (existsSync(imagePath)) {
-    wallpaper.set(imagePath);
-  }
-};
+// setTimeout(() => {
+//   downloadAndSetBg('https://beatconnect.io/bg/1041596/2176932/')
+//     .then(() => console.log('new bg set!'))
+//     .catch(err => console.log('failed', err.message));
+//   // wallpaper.set(path);
+// }, 5000);
 
-setTimeout(() => {
-  const path = normalize('C:\\Users\\AssAs\\Downloads\\téléchargé.jpg');
-  // console.log('setting wallpapapap');
-  downloadBg('https://beatconnect.io/bg/1076733/2256206/').then(() => console.log('dl term'));
-  // wallpaper.set(path);
-}, 5000);
-
-module.exports = { setWallpaper };
+module.exports = { setWallpaper: downloadAndSetBg };
