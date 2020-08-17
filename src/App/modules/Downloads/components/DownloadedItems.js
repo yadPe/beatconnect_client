c8afff30e6c1323527d8be5cb01ecf6895eefdfd
YadPe <petitot.py@gmail.com>
2020-08-17 19:30:28 +0200

    use VariableSizeList

--- src/App/modules/Downloads/components/DownloadedItems.js
+++ src/App/modules/Downloads/components/DownloadedItems.js
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
-import { FixedSizeList } from 'react-window';
+import { VariableSizeList } from 'react-window';
 import { connect } from 'react-redux';
 import { HistoryContext } from '../../../Providers/HistoryProvider';
 import DownloadsItem from './Item';
+import config from '../../../../shared/config';
 
 const DownloadedItems = ({ window }) => {
   const { history } = useContext(HistoryContext);
@@ -13,19 +14,19 @@ const DownloadedItems = ({ window }) => {
   });
   items.sort((a, b) => b.props.date - a.props.date);
 
-  const renderItems = ({ index, style }) => <div style={style}>{items[index]}</div>;
+  const renderItems = ({ index, style }) => (index === 0 ? <div /> : <div style={style}>{items[index - 1]}</div>);
   return (
     <div className="downloadMenu DownloadsItem" style={{ width: '100%' }}>
-      <FixedSizeList
+      <VariableSizeList
         height={window.height}
-        itemCount={items.length}
-        itemSize={130}
-        overscanCount={2}
+        itemCount={items.length + 1}
+        itemSize={index => (index === 0 ? config.display.topBarHeight : 130)}
+        overscanCount={5}
         width="100%"
         className="downloadMenu customScroll"
       >
         {items.length > 0 ? renderItems : 'The beatmaps you download will go here'}
-      </FixedSizeList>
+      </VariableSizeList>
     </div>
   );
 };
