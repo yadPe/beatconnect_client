ae35e204586c3688e38bbce61bdccf99eea5bc68
Yannis Petitot <46972108+yadPe@users.noreply.github.com>
2021-03-19 21:15:37 +0100

    feat(player): Add controls to side panel (#332)

--- src/App/helpers/renderIcons.js
+++ src/App/helpers/renderIcons.js
@@ -450,6 +450,17 @@ export default function({ name, color, width, height, style, secColor }) {
           />
         </svg>
       );
+    case 'Arrow':
+      return (
+        <svg x="0px" y="0px" width={width || '19px'} height={height || '19px'} viewBox="0 0 443.52 443.52">
+          <path
+            fill={fill}
+            d="M143.492,221.863L336.226,29.129c6.663-6.664,6.663-17.468,0-24.132c-6.665-6.662-17.468-6.662-24.132,0l-204.8,204.8
+			c-6.662,6.664-6.662,17.468,0,24.132l204.8,204.8c6.78,6.548,17.584,6.36,24.132-0.42c6.387-6.614,6.387-17.099,0-23.712
+			L143.492,221.863z"
+          />
+        </svg>
+      );
     case 'screenHeart':
       return (
         <svg x="0px" y="0px" width={width || '19px'} height={height || '19px'} viewBox="0 0 330.863 330.864">
---
+++ src/App/modules/common/NavPanel/SidePanel/MusicPlayer.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+import { connect } from 'react-redux';
+import config from '../../../../../shared/config';
+import { getThumbUrl } from '../../../../../shared/ppy.helpers';
+import renderIcons from '../../../../helpers/renderIcons';
+import { useAudioPlayer } from '../../../../Providers/AudioPlayerProvider.bs';
+import ScrollingText from '../../ScrollingText';
+
+const useStyle = createUseStyles({
+  playingSongWrapper: {
+    height: ({ expended }) => (expended ? '104px' : '44px'),
+  },
+  icon: {
+    marginRight: '8px',
+    height: '44px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+  },
+  expendedContentWrapper: {
+    fontSize: '15px',
+    color: 'white',
+    letterSpacing: '0.4pt',
+    padding: '0px 12px',
+  },
+  top: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    gap: '10px',
+  },
+  bottom: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  arrrowRight: {
+    '& > svg': {
+      transform: 'rotate(180deg)',
+    },
+    width: '19px',
+    height: '19px',
+    cursor: 'pointer',
+  },
+  arrrowLeft: {
+    width: '19px',
+    height: '19px',
+    cursor: 'pointer',
+  },
+  songImage: {
+    backgroundSize: 'cover',
+    borderRadius: '5px',
+    width: '60px',
+    height: '60px',
+    boxShadow: '5px 5px 20px rgba(190, 190, 190,0.15), -5px -5px 60px rgba(255, 254, 255,0.15)',
+    opacity: '0.8',
+    cursor: 'pointer',
+  },
+});
+
+const PlayingSong = ({ expended }) => {
+  const classes = useStyle({ expended });
+  const { playingState, togglePlayPause } = useAudioPlayer();
+  const visible = playingState.songTitle;
+
+  if (!visible) return null;
+  return (
+    <div className={classes.playingSongWrapper} onClick={togglePlayPause} role="tab">
+      <div className={classes.expendedContentWrapper}>
+        {expended && (
+          <div className={classes.top}>
+            <div className={classes.arrrowLeft}>{renderIcons({ name: 'Arrow' })}</div>
+
+            <div
+              className={classes.songImage}
+              style={{ backgroundImage: `url(${getThumbUrl(playingState.beatmapSetId)})` }}
+            />
+            <div className={classes.arrrowRight}>{renderIcons({ name: 'Arrow' })}</div>
+          </div>
+        )}
+        <div className={classes.bottom}>
+          <div className={classes.icon}>
+            {renderIcons({
+              name: playingState.isPlaying ? 'playButton' : 'pauseButton',
+              // color: osuIsRunning && theme.palette.primary.accent,
+              // secColor: osuIsRunning && '#e3609a',
+              width: '25px',
+              height: '25px',
+            })}
+          </div>
+          <div className={classes.label}>
+            <ScrollingText
+              text={playingState.songTitle}
+              maxWidth={`${config.display.sidePanelExpandedLength - 44 - 2}px`}
+              visible={expended}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ settings }) => ({ osuGamePath: settings.userPreferences.osuPath });
+export default connect(mapStateToProps)(PlayingSong);
