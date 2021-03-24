01c8f2449ea42aa5fe5bc506f49d10c93b24b12e
DEMARET Arnaut <arnaut.demaret@gmail.com>
2021-03-24 23:46:00 +0100

    feat: applied beatconnect logo as default audio player bg (#350)

--- src/App/modules/common/NavPanel/SidePanel/MusicPlayer.jsx
+++ src/App/modules/common/NavPanel/SidePanel/MusicPlayer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { connect } from 'react-redux';
 import config from '../../../../../shared/config';
+import reqImgAssets from '../../../../helpers/reqImgAssets';
 import { getThumbUrl } from '../../../../../shared/PpyHelpers.bs';
 import renderIcons from '../../../../helpers/renderIcons';
 import { useAudioPlayer } from '../../../../Providers/AudioPlayerProvider.bs';
@@ -83,7 +84,7 @@ const PlayingSong = ({ expended }) => {
             <div
               onClick={togglePlayPause}
               className={classes.songImage}
-              style={{ backgroundImage: `url(${getThumbUrl(playingState.beatmapSetId)})` }}
+              style={{ backgroundImage: `url(${getThumbUrl(playingState.beatmapSetId)}), url(${reqImgAssets(`./beatconnect_logo.png`)})`, backgroundSize : "auto, 75%", backgroundRepeat : "no-repeat" }}
             />
             <div className={classes.arrrowRight} onClick={handleNext}>
               {renderIcons({ name: 'Arrow' })}
