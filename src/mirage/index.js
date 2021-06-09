d79c369eeeff498b5f825d3b39cfd37374f3b5dc
YadPe <mwayannis@gmail.com>
2021-06-09 21:44:05 +0200

    mirage

--- src/mirage/index.js
+++ src/mirage/index.js
@@ -1,11 +1,30 @@
-import { createServer, Model, Factory } from 'miragejs';
+import { createServer } from 'miragejs';
 
 export function makeServer() {
   return createServer({
     routes() {
-      this.namespace = 'https://beatconnect.io/api';
+      this.timing = 0;
 
-      this.get('search');
+      this.get('https://beatconnect.io/api/search', async (schema, request) => {
+        const { p: page } = request.queryParams;
+
+        const mockName = `page${page}.json`;
+        const { default: mock } = await import(`./mocks/search/${mockName}`);
+
+        return mock;
+      });
+
+      this.get('https://beatconnect.io/api/packs/weekly', async () => {
+        const { default: mock } = await import(`./mocks/packs/weekly.json`);
+
+        return mock;
+      });
+
+      this.get('https://beatconnect.io/api/packs', async () => {
+        const { default: mock } = await import(`./mocks/packs/packs.json`);
+
+        return mock;
+      });
     },
   });
 }
