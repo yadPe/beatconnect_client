bc9fcf9e9d864c21faf2d73b0fc17ef1780ae58d
Yannis Petitot <46972108+yadPe@users.noreply.github.com>
2022-03-13 18:39:55 +0100

    perf(memory): Reduce usage when switching sections (#466)

--- src/App/app.actions.js
+++ src/App/app.actions.js
@@ -1,7 +1,19 @@
-export const changeCurrentSection = (sectionName, params = {}) => ({
-  type: 'UPDATEACTIVESECTION',
-  payload: { sectionName, params },
-});
+import { webFrame } from 'electron';
+
+/**
+ * Action creator will call clearCache as a side effect
+ * @param {string} sectionName
+ * @param {Record<string, any>} params
+ * @returns
+ */
+export const changeCurrentSection = (sectionName, params = {}) => {
+  // Used to clear web frame cache especially images when changing section since there is no real navigation in the app for chromium to to it by itself
+  webFrame.clearCache();
+  return {
+    type: 'UPDATEACTIVESECTION',
+    payload: { sectionName, params },
+  };
+};
 
 export const clearSectionParams = () => ({
   type: 'CLEAR_ACTIVE_SECTION_PARAMS',
