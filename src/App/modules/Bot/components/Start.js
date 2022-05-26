0e04ed3f7a6a3d557358ad3c09870c02049a86e7
Gumatan <arnaut.demaret@colisweb.com>
2022-05-26 17:17:58 +0200

    gist in browserwindow

--- src/App/modules/Bot/components/Start.js
+++ src/App/modules/Bot/components/Start.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import injectSheet from 'react-jss';
 import start from '../../../../Bot';
 import Toggle from '../../common/Toggle';
-import { getOsuApiKey } from '../../Settings/reducer/selectors'
+import { getOsuApiKey } from '../../Settings/reducer/selectors';
 
 const styles = {
   Start: {
@@ -24,15 +24,24 @@ const Start = ({ classes, connected, irc, osuApi }) => {
 
       <p>{connected ? (connected === 'connecting' ? 'Connecting to Bancho via IRC..' : 'Online') : 'Offline'}</p>
       {notReady ? (
-        <p style={{ margin: 'auto 10px' }}>
-          Warning: no irc credential found. Please go to settings section
-        </p>
+        <p style={{ margin: 'auto 10px' }}>Warning: no irc credential found. Please go to settings section</p>
       ) : null}
+      <button
+        onClick={_ => {
+          const remote = require('electron').remote;
+          const BrowserWindow = remote.BrowserWindow;
+          const win = new BrowserWindow({
+            height: 600,
+            width: 800,
+          });
+          win.loadFile("<script src='https://gist.github.com/yadPe/7d469d9dc40b1b1dda048f1171b421d5.js'></script>");
+        }}
+      ></button>
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   irc: state.settings.userPreferences.irc,
   osuApi: getOsuApiKey(state),
 });
