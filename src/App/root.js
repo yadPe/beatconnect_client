bbefe741a46b0f18fcd302e08e1b746eb0bf30a2
YadPe <petitot.py@gmail.com>
2022-05-27 17:15:13 +0200

    log

--- src/App/root.js
+++ src/App/root.js
@@ -20,7 +20,7 @@ dispatchOnResize();
 document.body.style.margin = 0;
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(
   <ErrorBoundary>
     <HistoryProvider>
